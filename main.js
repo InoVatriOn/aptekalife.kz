e97687dff7c714fec47f7ff370f2245c7120dcea
InoVatriOn <56877493+InoVatriOn@users.noreply.github.com>
2024-06-24 14:39:19 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ function addProductToCart(title, price, productImg) {
   var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
   for (var i = 0; i < cartItemsNames.length; i++) {
     if (cartItemsNames[i].innerText == title) {
-      alert("You have already add this item to cart");
+      alert("Вы уже добавили этот товар в корзину");
       return;
     }
   }
